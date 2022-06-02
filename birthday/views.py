b8ae3c259241ea4bccd2c286ab44c27c59892520
Katrina Harradine <katrina@revmo.app>
2022-06-02 10:21:45 -0400

    Birthday logic

--- birthday/views.py
+++ birthday/views.py
@@ -1,5 +1,9 @@
+from xmlrpc.client import NOT_WELLFORMED_ERROR
 from django.shortcuts import render
+import datetime 
 
 # Create your views here.
 def index(reuqest):
-    return render(reuqest, 'birthday/index.html')
+    now = datetime.datetime.now()
+    birthday = now.month == 9 and now.day == 27
+    return render(reuqest, 'birthday/index.html', {'birthday': birthday })
